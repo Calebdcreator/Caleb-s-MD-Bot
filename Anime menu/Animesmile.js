96530af989d9b67110682b14d881c2889916880d
CalebMD <ubachukwuirunna@gmail.com>
2025-08-07 22:05:42 +0100

    Update Animesmile.js

--- Anime menu/Animesmile.js
+++ Anime menu/Animesmile.js
@@ -1,1 +1,9 @@
+import fetch from 'node-fetch'
 
+async function handleAnimeSmile(sock, msg) {
+  const res = await fetch('https://api.waifu.pics/sfw/smile')
+  const json = await res.json()
+  await sock.sendMessage(msg.key.remoteJid, { image: { url: json.url }, caption: '😊 Anime Smile' }, { quoted: msg })
+}
+
+export default handleAnimeSmile
